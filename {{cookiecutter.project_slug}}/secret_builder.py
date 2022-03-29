7d715dcf4d648f067c569ede4fce8e1ec6e12f60
EverydayFuture <admin@theeverydayfuture.com>
2022-03-29 14:35:48 -0600

    fix: separate secret build for api and frontend

--- {{cookiecutter.project_slug}}/secret_builder.py
+++ {{cookiecutter.project_slug}}/secret_builder.py
@@ -15,15 +15,12 @@ def build(local_config='docker'):
         configs = json.load(fp)
     template = Environment(loader=BaseLoader(), trim_blocks=True, lstrip_blocks=True,
                            undefined=StrictUndefined).from_string(template_text)
-    for env_name, values in configs.items():
-        values.update({'version': global_config.VERSION})
-        with open(os.path.join(global_config.PROJECT_DIR, f'secret-{env_name}.env'), 'w') as fp:
-            fp.write(template.render(**values))
-    local_configs = configs[local_config]
-    local_configs.update({'version': global_config.VERSION})
     # Create the local local.env file
-    with open(os.path.join(global_config.PROJECT_DIR, 'local.env'), 'w') as fp:
-        fp.write(template.render(**local_configs))
+    for app in ['api-', 'frontend-']:
+        local_configs = configs[app + local_config]
+        local_configs.update({'version': global_config.VERSION})
+        with open(os.path.join(global_config.PROJECT_DIR, 'local.env'), 'w') as fp:
+            fp.write(template.render(**local_configs))
     print(f'\n\n\n\n-------------------------------------------------------')
     print(f'Rebuilt local secrets for MODE == {local_config}')
     print(f'-------------------------------------------------------\n\n')
@@ -33,6 +30,6 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         target_config = sys.argv[1]
     else:
-        target_config = 'api-docker'
+        target_config = 'docker'
     # Build all the .env files from the template and secrets
     build(target_config)
